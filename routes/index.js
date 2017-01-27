aeda429aeb2f8a75968316350edc5a8659b494bc
nihas <nihas@qburst.com>
2017-01-27 19:17:53 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+router.post('/upload', upload.single('movieAvatar'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    console.log(req.body);
+    console.log(req.file);
+    res.redirect('/');
+})
+
+module.exports = router;
---
+++ routes/register.js
@@ -0,0 +1,16 @@
+router.post('/register', function(req, res, next) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { error : err.message });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
